8117230957c67923523bffb843b3f9a5aaea94e0
Adrian Mihaila | eMAG <adrian.mihaila@emag.ro>
2018-05-25 22:33:31 +0300

    Add first commit.

---
+++ src/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrian.mihaila
+ * Date: 5/25/2018
+ * Time: 10:10 PM
+ */
---
+++ tests/FunctionWaitTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrian.mihaila
+ * Date: 5/25/2018
+ * Time: 10:28 PM
+ */
+
+class FunctionWaitTest {
+
+}
