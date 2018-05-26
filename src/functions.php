a7e72aaf948d5f869474c1dd0a80156cd76ba7b5
Adi Mihaila <adrian.mihaila@emag.ro>
2018-05-26 11:29:14 +0300

    Add support for a closure which returns a promise.

--- src/functions.php
+++ src/functions.php
@@ -33,3 +33,35 @@ function wait(PromiseInterface $promise) {
 
     return $result;
 }
+
+function waitClosure(\Closure $closure) {
+    $result = null;
+    $exception = null;
+    $isRejected = false;
+
+    $wait = true;
+    while ($wait) {
+        $closure()
+            ->then(function ($r) use (&$result) {
+                $result = $r;
+            }, function ($e) use (&$exception, &$isRejected) {
+                $exception = $e;
+                $isRejected = true;
+            })
+            ->always(function () use (&$wait) {
+                $wait = false;
+            });
+    }
+
+    if ($isRejected) {
+        if (!$exception instanceof \Exception) {
+            $exception = new \UnexpectedValueException(
+                'Promise rejected with unexpected value of type ' . (is_object($exception) ? get_class($exception) : gettype($exception))
+            );
+        }
+
+        throw $exception;
+    }
+
+    return $result;
+}
